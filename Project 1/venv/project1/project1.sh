d2de372c53a81653f39d16dfe8734ae69791acc3
bgv26 <bgv26@mail.ru>
2018-07-27 18:16:26 +0300

    Create .gitignore

--- Project 1/venv/project1/project1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export FLASK_APP="application.py"
-export FLASK_DEBUG="1"
-export DATABASE_URL="postgres://jijyckscjqzzbt:4d91d7015888ab23de68ed8aced081ad89ca643a2852ab5b6d7032051e12e133@ec2-54-83-204-230.compute-1.amazonaws.com:5432/dacpmmhnr8enn1"
-export GOODREADS_KEY="4azJe5CQqg8hi3c58lAgaw"
